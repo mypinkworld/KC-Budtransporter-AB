7912a5201e05a2e3c74ef86918b931385f49688a
mypinkworld <myworkplace.art@gmail.com>
2024-09-05 02:18:57 +0200

    box box box

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function toggleMenu() {
     var menu = document.querySelector('.nav-menu');
     menu.classList.toggle('open');
 }
+
+function toggleText(spanId) {
+    var text = document.getElementById(spanId);
+    var button = document.querySelector(`button[onclick="toggleText('${spanId}')"]`);
+    
+    if (text.style.display === "none" || text.style.display === "") {
+        text.style.display = "block"; // Visa texten
+        button.textContent = "Se mindre"; // Ändra texten
+    } else {
+        text.style.display = "none"; // Dölj texten
+        button.textContent = "Se mer"; // Återställ
+    }
+}
